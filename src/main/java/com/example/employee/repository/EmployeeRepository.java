36750b3c2e0816c2c181fa1b79f7c1f653989011
lh <isixline@qq.com>
2019-06-14 11:22:04 +0800

    change some

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,14 +1,14 @@
 package com.example.employee.repository;
 
 import com.example.employee.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
+
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
@@ -20,20 +20,21 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //2.找出Employee表中第一个姓名包含`*`字符并且薪资大于*的雇员个人信息
     public Employee findFirstByNameContainingAndSalaryIsGreaterThan(String name, int salary);
 
+    public List<Employee> findByCompanyId(int compnyId);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
     public Employee findFirstByCompanyIdOrderBySalaryDesc(int companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
-    Page<Employee> findAll(Pageable pageable);
+    public Page<Employee> findAll(Pageable pageable);
 
     //5.查找**的所在的公司的公司名称
-    @Query("SELECT companyName FROM company WHERE id IN (SELECT companyId FROM employee WHERE name = ?1)")
+    @Query(value = "SELECT companyName FROM company WHERE id IN (SELECT companyId FROM employee WHERE name = ?1)", nativeQuery = true)
     public String findCompanyNameByName(String name);
 
     //6.将*的名字改成*,输出这次修改影响的行数
     @Modifying
-    @Query("UPDATE employee SET name = ?1 WHERE name = ?2")
+    @Query(value = "UPDATE employee SET name = ?1 WHERE name = ?2", nativeQuery = true)
     public int updateName(String newName, String oldName);
 
     //7.删除姓名是*的employee
